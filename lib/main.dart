8424d373781cb54b1458c28e2a53387eff3a0612
taeto <skystaeto@naver.com>
2022-09-06 20:22:16 +0900

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class _LogInState extends State<LogIn> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Log in'),
+        title: Text('Log in with git'),
         backgroundColor: Colors.redAccent,
         centerTitle: true,
         leading: IconButton(icon: Icon(Icons.menu), onPressed: () {}),
@@ -37,39 +37,43 @@ class _LogInState extends State<LogIn> {
         ],
       ),
       body: Builder(
-        builder: (ctx)=> GestureDetector(
-          onTap: (){FocusScope.of(context).unfocus();},
-          child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  Padding(padding: EdgeInsets.only(top: 50)),
-                  Center(
-                    child: Image(
-                      image: AssetImage('image/chef.gif'),
-                      width: 170,
-                      height: 190,
-                    ),
-                  ),
-                  Form(
-                      child: Theme(
+          builder: (ctx) => GestureDetector(
+                onTap: () {
+                  FocusScope.of(context).unfocus();
+                },
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Padding(padding: EdgeInsets.only(top: 50)),
+                      Center(
+                        child: Image(
+                          image: AssetImage('image/chef.gif'),
+                          width: 170,
+                          height: 190,
+                        ),
+                      ),
+                      Form(
+                          child: Theme(
                         data: ThemeData(
                             primaryColor: Colors.red,
                             inputDecorationTheme: InputDecorationTheme(
-                                labelStyle: TextStyle(color: Colors.teal, fontSize: 25))),
+                                labelStyle: TextStyle(
+                                    color: Colors.teal, fontSize: 25))),
                         child: Container(
                           padding: EdgeInsets.all(40),
                           child: Column(
                             children: [
-                              if(emailField)
+                              if (emailField)
+                                TextField(
+                                    autofocus: true,
+                                    decoration: InputDecoration(
+                                        labelText: 'Enter dice'),
+                                    keyboardType: TextInputType.emailAddress,
+                                    controller: controller1,
+                                    key: ValueKey(1)),
                               TextField(
-                                autofocus: true,
-                                decoration: InputDecoration(labelText: 'Enter dice'),
-                                keyboardType: TextInputType.emailAddress,
-                                controller: controller1,
-                                 key: ValueKey(1)
-                              ),
-                              TextField(
-                                decoration: InputDecoration(labelText: 'Enter password'),
+                                decoration: InputDecoration(
+                                    labelText: 'Enter password'),
                                 keyboardType: TextInputType.text,
                                 obscureText: true,
                                 key: ValueKey(2),
@@ -83,9 +87,8 @@ class _LogInState extends State<LogIn> {
                                   height: 50,
                                   child: ElevatedButton(
                                       style: ElevatedButton.styleFrom(
-                                        primary: Colors.purple,
-                                        minimumSize: Size(150,50)
-                                      ),
+                                          primary: Colors.purple,
+                                          minimumSize: Size(150, 50)),
                                       child: Icon(
                                         //Icons.arrow_forward,
                                         Icons.visibility_off,
@@ -93,14 +96,24 @@ class _LogInState extends State<LogIn> {
                                         size: 35,
                                       ),
                                       onPressed: () {
-                                        if(controller1.text == 'dice' && controller2.text == '1323'){
-                                          Navigator.push(context,
-                                              MaterialPageRoute(builder: (context)=>Dice()));
-                                        } if(controller1.text =='dice' && controller2.text !='1323'){
+                                        if (controller1.text == 'dice' &&
+                                            controller2.text == '1323') {
+                                          Navigator.push(
+                                              context,
+                                              MaterialPageRoute(
+                                                  builder: (context) =>
+                                                      Dice()));
+                                        }
+                                        if (controller1.text == 'dice' &&
+                                            controller2.text != '1323') {
                                           showSnackBar2(ctx);
-                                        } if(controller1.text !='dice' && controller2.text =='1323'){
+                                        }
+                                        if (controller1.text != 'dice' &&
+                                            controller2.text == '1323') {
                                           showSnackBar3(ctx);
-                                        }else {showSnackBar1(ctx);}
+                                        } else {
+                                          showSnackBar1(ctx);
+                                        }
                                         setState(() {
                                           //emailField = false;
                                         });
@@ -109,36 +122,43 @@ class _LogInState extends State<LogIn> {
                           ),
                         ),
                       )),
-                ],
-              ),
-            ),
-        )
-      ),
+                    ],
+                  ),
+                ),
+              )),
     );
   }
 }
-void showSnackBar1(BuildContext context){
-  Scaffold.of(context).showSnackBar(SnackBar(
-      content: Text('로그인정보를 다시 확인하세요',
-        textAlign: TextAlign.center,),
-        duration: Duration(seconds: 2),
-        backgroundColor: Colors.blue,));
-}
 
-void showSnackBar2(BuildContext context){
+void showSnackBar1(BuildContext context) {
   Scaffold.of(context).showSnackBar(SnackBar(
-    content: Text('비밀번호가 일치하지않습니다',
-      textAlign: TextAlign.center,),
+    content: Text(
+      '로그인정보를 다시 확인하세요',
+      textAlign: TextAlign.center,
+    ),
     duration: Duration(seconds: 2),
-    backgroundColor: Colors.blue,));
+    backgroundColor: Colors.blue,
+  ));
 }
 
-void showSnackBar3(BuildContext context){
+void showSnackBar2(BuildContext context) {
   Scaffold.of(context).showSnackBar(SnackBar(
-    content: Text('dice의 철자를 확인하세요',
-      textAlign: TextAlign.center,),
+    content: Text(
+      '비밀번호가 일치하지않습니다',
+      textAlign: TextAlign.center,
+    ),
     duration: Duration(seconds: 2),
-    backgroundColor: Colors.blue,));
+    backgroundColor: Colors.blue,
+  ));
 }
 
-
+void showSnackBar3(BuildContext context) {
+  Scaffold.of(context).showSnackBar(SnackBar(
+    content: Text(
+      'dice의 철자를 확인하세요',
+      textAlign: TextAlign.center,
+    ),
+    duration: Duration(seconds: 2),
+    backgroundColor: Colors.blue,
+  ));
+}
